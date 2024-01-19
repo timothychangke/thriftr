e2bf1d426fe2bb4d7ddda67651208f4fb2470093
Joel <jlee327@e.ntu.edu.sg>
2024-01-20 01:08:34 +0800

    homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,15 @@
+import { NavLink } from 'react-router-dom';
+import classes from '../styles/Home.module.css';
+
 function HomePage() {
     return (
         <>
-            <main>
-                <h1>My Home Page</h1>
+            <main className={classes.main}>
+                <h1 className={classes.h1}>Thriftr - Fall in love with Preloved items</h1>
+                <div>Immerse yourself in a unique online shopping experience that combines the charm of yesterday with the convenience of modern technology
+                <br/>
+                <button className={classes.button}><NavLink to="/products">Start Shopping</NavLink></button>
+                </div>
             </main>
         </>
     );
